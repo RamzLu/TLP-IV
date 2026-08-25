04c5843171a63a9d3a3281f2659441c7458c9f0d
RamzLu <luanaabigail168@gmail.com>
2026-08-25 12:43:22 -0300

    se cero una clase soporte que actua como observador y recibe notificaciones

--- patrones-diseños/ej3.ts
+++ patrones-diseños/ej3.ts
@@ -1,3 +1,9 @@
 interface Observador {
   actualizar(mensaje: string): void;
 }
+
+class Soporte implements Observador {
+  public actualizar(mensaje: string): void {
+    console.log(`Soporte notificado: ${mensaje}`);
+  }
+}
