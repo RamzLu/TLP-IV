f527d5cab95c3a41608440dc34a3068b7aecf1bf
RamzLu <luanaabigail168@gmail.com>
2026-08-25 12:07:16 -0300

    hice lo mismo para desktop y servidor

--- patrones-diseños/ej2.ts
+++ patrones-diseños/ej2.ts
@@ -13,3 +13,27 @@ class Notebook implements Equipo2 {
     return `Tipo: Notebook, nombre: ${this.nombre}, ram: ${this.ram}, procesador ${this.procesador}`;
   }
 }
+
+class Desktop implements Equipo2 {
+  constructor(
+    private nombre: string,
+    private ram: string,
+    private procesador: string,
+  ) {}
+
+  public datelles(): string {
+    return `Tipo: Desktop, nombre: ${this.nombre}, ram: ${this.ram}, procesador ${this.procesador}`;
+  }
+}
+
+class Servidor implements Equipo2 {
+  constructor(
+    private nombre: string,
+    private ram: string,
+    private procesador: string,
+  ) {}
+
+  public datelles(): string {
+    return `Tipo: Servidor, nombre: ${this.nombre}, ram: ${this.ram}, procesador ${this.procesador}`;
+  }
+}
