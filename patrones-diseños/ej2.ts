391e0899875740db3d30cd8f4668fe76717455c1
RamzLu <luanaabigail168@gmail.com>
2026-08-25 12:12:02 -0300

    se hizo la clase EquipoFactory con el switch para describir al esquipo segun el tipo (Notebook, Desktop, Servidor)

--- patrones-diseños/ej2.ts
+++ patrones-diseños/ej2.ts
@@ -37,3 +37,23 @@ class Servidor implements Equipo2 {
     return `Tipo: Servidor, nombre: ${this.nombre}, ram: ${this.ram}, procesador ${this.procesador}`;
   }
 }
+
+class EquipoFactory {
+  public crearEquipo(
+    tipo: string,
+    nombre: string,
+    ram: string,
+    procesador: string,
+  ): Equipo2 {
+    switch (tipo) {
+      case "Notebook":
+        return new Notebook(nombre, ram, procesador);
+      case "Desktop":
+        return new Desktop(nombre, ram, procesador);
+      case "Servidor":
+        return new Servidor(nombre, ram, procesador);
+      default:
+        throw new Error(`Tipo de equipo no valido`);
+    }
+  }
+}
