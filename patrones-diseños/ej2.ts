e0e2d67494636d408cfcf60c29d054d63c2d2f30
RamzLu <luanaabigail168@gmail.com>
2026-08-25 12:01:42 -0300

    hice la classe notebook con su constructor con nombre, ram, y procesador y tambien un metodo para detallar el equipo

--- patrones-diseños/ej2.ts
+++ patrones-diseños/ej2.ts
@@ -1,3 +1,15 @@
 interface Equipo2 {
   datelles(): string;
 }
+
+class Notebook implements Equipo2 {
+  constructor(
+    private nombre: string,
+    private ram: string,
+    private procesador: string,
+  ) {}
+
+  public datelles(): string {
+    return `Tipo: Notebook, nombre: ${this.nombre}, ram: ${this.ram}, procesador ${this.procesador}`;
+  }
+}
